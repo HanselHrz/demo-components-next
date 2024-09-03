d6c711b1807d07605d8d0833d2b75e01f892f0ef
HanselHrz <hansel.hernandezc@gmail.com>
2024-09-03 01:20:28 -0600

    setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import "lynx-ui-demo-components/dist/style.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
